1d3d0183de4e777848f0331ed34a65b076e89c11
User <naumenko.gs@gmail.com>
2019-01-03 17:11:46 -0800

    fix

--- src/applications/model/bitcoin-node.cc
+++ src/applications/model/bitcoin-node.cc
@@ -935,10 +935,11 @@ void BitcoinNode::SaveTxData(int txId, Ipv4Address from) {
 void BitcoinNode::AddToReconciliationSets(int txId, Ipv4Address from) {
   if (m_timeToRun < Simulator::Now().GetSeconds() + timeNotToCount)
     return;
+
   // std::cout << "Node " << m_nodeStats->nodeId << " adds tx: " << txId << "from peer" << from << std::endl;
   for (std::vector<Ipv4Address>::const_iterator i = m_peersAddresses.begin(); i != m_peersAddresses.end(); ++i)
   {
-    if (*i == from) {
+    if (*i == from || peersMode[*i] == BLACK_HOLE) {
       continue;
     }
     m_peerReconciliationSets[*i].push_back(txId);
